b9798fc9674edd122473e618cfb9ac1101f312ca
MrWakeCZ <joseff.kral@gmail.com>
2018-01-27 21:10:59 +0100

    Úprava zpráv v SQL kontrole

--- src/main/java/cz/wake/craftbungee/managers/SQLChecker.java
+++ src/main/java/cz/wake/craftbungee/managers/SQLChecker.java
@@ -1,31 +1,33 @@
 package cz.wake.craftbungee.managers;
 
 import cz.wake.craftbungee.Main;
+import net.md_5.bungee.api.ChatColor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 public class SQLChecker implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("[CraftBungee] Kontrola pripojeni do databaze.");
+        Main.getInstance().getLogger().log(Level.INFO, ChatColor.YELLOW + "Kontrola pripojeni do databaze.");
         try (Connection c = Main.getInstance().getSQLManager().getPool().getConnection()) {
             try {
                 if (c == null || c.isClosed()) {
-                    System.out.println("[CraftBans] Pripojeni do SQL je NULL!");
+                    Main.getInstance().getLogger().log(Level.INFO, ChatColor.RED + "Pripojeni do SQL je NULL!");
                     if (Main.getInstance().getSQLManager().getPool().getDataSource() != null) {
                         Main.getInstance().getSQLManager().getPool().getDataSource().close();
                     }
                     Main.getInstance().getSQLManager().getPool().setupPool();
                 } else {
-                    System.out.println("[CraftBans] Pripojeni do SQL je OK!");
+                    Main.getInstance().getLogger().log(Level.INFO, ChatColor.GREEN + "Pripojeni do SQL je OK!");
                 }
             } finally {
                 c.close();
             }
         } catch (SQLException ex) {
-            System.out.println("[CraftBans] Pripojeni do SQL je NULL! Chyba:");
+            Main.getInstance().getLogger().log(Level.INFO, ChatColor.RED + "Pripojeni do SQL je NULL! Chyba:");
             ex.printStackTrace();
         }
     }
