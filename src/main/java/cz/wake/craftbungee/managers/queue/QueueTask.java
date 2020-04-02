65b41bfa93330b71cd2ededb77ec1da3e010a2c9
igniss <methalicity@gmail.com>
2020-04-02 20:56:22 +0200

    Vymazanie zbytočných podmienok v QueueTask

--- src/main/java/cz/wake/craftbungee/managers/queue/QueueTask.java
+++ src/main/java/cz/wake/craftbungee/managers/queue/QueueTask.java
@@ -1,11 +1,10 @@
 package cz.wake.craftbungee.managers.queue;
 
 import cz.wake.craftbungee.Main;
+import cz.wake.craftbungee.utils.Logger;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class QueueTask implements Runnable {
-
-    private ProxiedPlayer previousPlayer;
 
     @Override
     public void run() {
@@ -14,10 +13,10 @@ public class QueueTask implements Runnable {
             java.util.Queue<ProxiedPlayer> queue = craftQueue.getQueue();
             for (int i = 0; i <= craftQueue.getMaxPerWave(); i++) {
                 ProxiedPlayer chosenPlayer = queue.peek();
-                if (previousPlayer == chosenPlayer) continue;
-                previousPlayer = chosenPlayer;
                 if (chosenPlayer == null) continue;
+                if (!chosenPlayer.isConnected()) continue;
                 craftQueue.connectPlayer(chosenPlayer, false);
+                Logger.info("Connecting queued player '" + chosenPlayer.getDisplayName() + "' to '" + craftQueue.getServerName() + "'.");
             }
         }
     }
